89286066b0f50d102942fed80a7e7d59e9de8c4c
ElPulpitoManotas <hdrauxiliar@gmail.com>
2016-11-12 01:09:58 -0300

    Create lsmc.py
    Commit primera version del lsmc.py, el main corre el ejemplo de Longstaff-Schwartz

---
+++ lsmc.py
@@ -0,0 +1,102 @@
+## @Date: November 12, 2016
+
+import pandas as pd
+import numpy as np
+
+
+## ----------------------------------------------------------------------------
+def LSMC_american(
+            mc, 
+            r = 0.06,
+            strike = 1.10,
+            degree = 2,
+            option = 'put'):
+    """
+    Input: a pandas.DataFrame with the Monte Carlo simulation n_paths
+    Output: the Option cash flow matrix, and the american option path
+    """
+
+    n_paths = len(mc)
+    n_T = len(mc.columns)
+    nan = pd.DataFrame(np.empty((n_paths, n_T,)))
+    nan[:] = np.nan
+
+    cashflows = pd.DataFrame(0, index=mc.index, columns=mc.columns)
+
+    zero = pd.DataFrame(np.zeros_like(mc[3]))
+
+    ## Discount factor
+    df = np.exp(-r)
+
+    for i in xrange(n_T):
+
+        t = n_T - i - 1
+
+        ## Price of immediate exercise a time 't' for american call
+        if option == 'call':
+            exercise = np.maximum(mc[t] - strike, 0)
+        elif option == 'put':
+            exercise = np.maximum(strike - mc[t], 0)
+
+        ## Is in-the-money if exercise price is positive
+        ITM = (exercise > 0)
+        mask = ITM[ITM].index
+        #print(mask)
+
+        if i > 0:
+            ## X: stock prices at time t, only if they are in the money
+            ## Y: denote the discounted cashflows discounted one step back
+            X = nan.copy(deep=True)
+            Y = nan.copy(deep=True)
+            X = mc.loc[ITM, t]
+            Y = cashflows.loc[X.index, t+1]*df
+            p = np.polyfit(X, Y, degree)
+
+            ## "The value of continuation is given by substituting X into
+            ## the conditional expectation function"
+            continuation = pd.Series(np.polyval(p, X), index=X.index)
+
+            ## Paths that are early exeercized
+            mask = (exercise.loc[ITM] > continuation)
+            mask = mask[mask].index
+
+        ## Cashflows
+        ## Override future cashflows, if it's early exercized
+        cashflows.loc[mask, t] = exercise[mask]
+        for ti in xrange(t+1, n_T):
+            cashflows.loc[mask, ti] = 0
+        #print('cashflows at t={:d}'.format(t))
+        #print(cashflows)
+
+    ## Calculate the price
+    discount_cashflows = cashflows.copy(deep=True)
+    for i in xrange(len(cashflows.columns)):
+        ## Discount factor
+        df = np.exp(-i*r)
+        discount_cashflows[i] = cashflows[i]*df
+
+    price = discount_cashflows.sum().sum() / n_paths
+
+    return cashflows, price
+
+
+
+
+
+## ----------------------------------------------------------------------------
+if __name__ == "__main__":
+
+    mc = [[1.00, 1.09, 1.08, 1.34],
+          [1.00, 1.16, 1.26, 1.54],
+          [1.00, 1.22, 1.07, 1.03],
+          [1.00, 0.93, 0.97, 0.92],
+          [1.00, 1.11, 1.56, 1.52],
+          [1.00, 0.76, 0.77, 0.90],
+          [1.00, 0.92, 0.84, 1.01],
+          [1.00, 0.88, 1.22, 1.34]]
+    mc = pd.DataFrame(mc)
+
+    cashflows, price = LSMC_american(mc)
+
+    print(cashflows)
+    print(price)
